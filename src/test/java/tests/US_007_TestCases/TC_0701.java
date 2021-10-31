f5d99ee5ba9f514d5c6c7940540ed20c74d12de2
Cagatay <cagataybayramgonen@gmail.com>
2021-10-31 13:17:21 +0300

    version

--- src/test/java/tests/US_007_TestCases/TC_0701.java
+++ src/test/java/tests/US_007_TestCases/TC_0701.java
@@ -1,4 +1,10 @@
 package tests.US_007_TestCases;
 
+import org.testng.annotations.Test;
+
 public class TC_0701 {
+    @Test
+    public void test(){
+        System.out.println("ilk test");
+    }
 }
