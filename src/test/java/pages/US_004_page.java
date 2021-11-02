ce13f13ce60cc13759d736ac2786440e47876fa0
Gökhan <gokhancebisli68@gmail.com>
2021-11-02 17:30:28 +0100

    gokhan

--- src/test/java/pages/US_004_page.java
+++ src/test/java/pages/US_004_page.java
@@ -1,5 +1,7 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,4 +9,29 @@ public class US_004_page {
     public US_004_page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(xpath="//li[@id='navLogon']")
+    public WebElement ilkLoginLinki;
+
+    @FindBy(id="UserName")
+    public WebElement usernameKutusu;
+
+    @FindBy(id="Password")
+    public WebElement passwordKutusu;
+
+    @FindBy(id="btnSubmit")
+    public WebElement loginButonu;
+
+    @FindBy(xpath="//li[@id='menuHotels']")
+    public WebElement HotelMenüsü;
+
+    @FindBy(xpath = "//span[text()='Hotel Management']")
+    public WebElement hotelManagementLinki;
+
+    @FindBy(xpath = "//span[text()='Add Hotel ']")
+    public WebElement addHotelLink;
+
+    @FindBy(partialLinkText = "Hotel List")
+    public WebElement hotelListLink;
+
 }
