a03c8ec5f79821eb966dad5367a44e5e39f0285a
yavuz <celikselim14@gmail.com>
2021-11-02 19:13:49 +0100

    birinci ve ikinci test sonu

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver=new ChromeDriver();
+
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
