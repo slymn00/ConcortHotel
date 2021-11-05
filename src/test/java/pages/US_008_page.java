b71a3a719c44e927293d973b90694cc5bf33bf84
CumaAli <semihmuhlis6304@gmail.com>
2021-11-05 22:08:56 +0100

    cok sukur projemiz tamamlandi

--- src/test/java/pages/US_008_page.java
+++ src/test/java/pages/US_008_page.java
@@ -1,5 +1,8 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import utilities.Driver;
 
@@ -7,4 +10,53 @@ public class US_008_page {
     public US_008_page(){
         PageFactory.initElements(Driver.getDriver(),this);
     }
+
+    @FindBy(xpath = "//a[text()='Log in']")
+    public WebElement LoginButonu;
+    @FindBy(id = "UserName")
+    public WebElement userName;
+    @FindBy(id = "Password")
+    public WebElement Password;
+    @FindBy(id = "btnSubmit")
+    public WebElement Login;
+    @FindBy(linkText = "Hotel Management")
+    public WebElement HotelManagement;
+    @FindBy(xpath = "/html/body/div[3]/div[1]/div/ul/li[3]/ul/li[2]/ul/li[3]/a")
+    public WebElement RoomReservations;
+    @FindBy(className = "hidden-480")
+    public WebElement AddRoomReservation;
+    @FindBy(id = "IDUser")
+    public WebElement IDUser;
+    @FindBy(id = "IDHotelRoom")
+    public WebElement IDUser2;
+    @FindBy(id = "Price")
+    public WebElement Price;
+    @FindBy(id = "DateStart")
+    public WebElement DateStart;
+    @FindBy(id = "DateEnd")
+    public WebElement DateEnd;
+    @FindBy(id = "AdultAmount")
+    public WebElement AdultAmount;
+    @FindBy(id = "ChildrenAmount")
+    public WebElement ChildrenAmount;
+    @FindBy(id = "ContactNameSurname")
+    public WebElement ContactNameSurname;
+    @FindBy(id = "ContactPhone")
+    public WebElement ContactPhone;
+    @FindBy(id = "ContactEmail")
+    public WebElement ContactEmail;
+    @FindBy(id = "Notes")
+    public WebElement Notes;
+    @FindBy(id = "uniform-Approved")
+    public WebElement Approved;
+    @FindBy(id = "uniform-IsPaid")
+    public WebElement IsPaid;
+    @FindBy(id = "btnSubmit")
+    public WebElement SAVE;
+    @FindBy(className = "bootbox-body")
+    public WebElement successfully;
+
+
+
+
 }
--- src/test/java/tests/US_008_TestCases/TC_0801.java
+++ src/test/java/tests/US_008_TestCases/TC_0801.java
@@ -1,4 +1,72 @@
 package tests.US_008_TestCases;
 
+import com.github.javafaker.Faker;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import pages.US_008_page;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import java.util.Date;
+
 public class TC_0801 {
+    @Test
+    public void room_reservations() throws InterruptedException {
+        US_008_page us_008_page=new US_008_page();
+
+        Driver.getDriver().get(ConfigReader.getProperty("US_08_ConcortHotelUrl"));
+        us_008_page.LoginButonu.click();
+
+        us_008_page.userName.sendKeys(ConfigReader.getProperty("us_08_userName"));
+        us_008_page.Password.sendKeys(ConfigReader.getProperty("us_08_Password"));
+        us_008_page.Login.click();
+        us_008_page.HotelManagement.click();
+        us_008_page.RoomReservations.click();
+        us_008_page.AddRoomReservation.click();
+        Thread.sleep(1000);
+        Select select=new Select(us_008_page.IDUser);
+        select.selectByVisibleText("manager");
+        Select select2=new Select(us_008_page.IDUser2);
+        select2.selectByVisibleText("Guzelbahce");
+        us_008_page.IDUser2.sendKeys(Keys.TAB);
+        Faker faker=new Faker();
+        us_008_page.Price.sendKeys(faker.commerce().price());
+        us_008_page.Price.sendKeys(Keys.TAB);
+        Thread.sleep(1000);
+        us_008_page.DateStart.sendKeys("01-01-2022");
+        us_008_page.DateStart.sendKeys(Keys.TAB);
+        us_008_page.DateEnd.sendKeys("12-01-2022");
+        us_008_page.DateEnd.sendKeys(Keys.TAB);
+        us_008_page.AdultAmount.sendKeys(faker.number().digit());
+        us_008_page.AdultAmount.sendKeys(Keys.TAB);
+        us_008_page.ChildrenAmount.sendKeys(faker.number().digit());
+        us_008_page.ChildrenAmount.sendKeys(Keys.TAB);
+        us_008_page.ContactNameSurname.sendKeys(faker.name().fullName());
+        us_008_page.ContactNameSurname.sendKeys(Keys.TAB);
+        us_008_page.ContactPhone.sendKeys(faker.phoneNumber().phoneNumber());
+        us_008_page.ContactPhone.sendKeys(Keys.TAB);
+        us_008_page.ContactEmail.sendKeys(faker.internet().emailAddress());
+        us_008_page.ContactEmail.sendKeys(Keys.TAB);
+        us_008_page.Notes.sendKeys("Hotelinizde ilk defa konakladik ve arkadaslar olarak gercekten cok memnun kaldik.");
+        us_008_page.Notes.sendKeys(Keys.TAB);
+        us_008_page.Approved.click();
+        //us_008_page.Approved.sendKeys(Keys.TAB);
+        us_008_page.IsPaid.click();
+        //us_008_page.IsPaid.sendKeys(Keys.TAB);
+        us_008_page.SAVE.click();
+
+        SoftAssert softAssert = new SoftAssert();
+
+        softAssert.assertTrue(us_008_page.successfully.isDisplayed(),"beklenen RoomReservation was inserted successfully yazisi cikmadi");
+
+    }
+
+    @AfterClass
+    public void teardown(){
+        Driver.closeDriver();
+    }
 }
