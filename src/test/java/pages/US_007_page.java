b64cca0d0d8630a56bd81ed872f221cd4c1461d6
Cagatay <cagataybayramgonen@gmail.com>
2021-11-06 14:48:20 +0300

    TC_703_Cagatay

--- src/test/java/pages/US_007_page.java
+++ src/test/java/pages/US_007_page.java
@@ -38,6 +38,36 @@ public class US_007_page {
     @FindBy(xpath = "(//a[@class='btn btn-xs default'])[1]")
     public WebElement firstHotelRoomDetailBttn;
 
+    @FindBy(id = "IDHotel")
+    public WebElement idHotelDropdownMenu;
+
+    @FindBy(id = "Code")
+    public WebElement codeBox;
+
+    @FindBy(id = "Name")
+    public WebElement nameBox;
+
+    @FindBy(id = "Location")
+    public WebElement locationBox;
+
+    @FindBy(xpath = "//textarea[@title='Zengin Metin Editörü, Description']")
+    public WebElement descriptionTextArea;
+
+    @FindBy(id = "Price")
+    public WebElement priceBox;
+
+    @FindBy(id = "IDGroupRoomType")
+    public WebElement idGroupRoomTypeDropdownMenu;
+
+    @FindBy(id = "MaxAdultCount")
+    public WebElement maxAdultCountBox;
+
+    @FindBy(id = "MaxChildCount")
+    public WebElement maxChildCountBox;
+
+    @FindBy(id = "IsAvailable")
+    public WebElement isAvailableBox;
+
     public void login(){
         Driver.getDriver().get(ConfigReader.getProperty("US_007_MainPageUrl"));
         loginBttn.click();
--- src/test/java/tests/US_007_TestCases/TC_0703.java
+++ src/test/java/tests/US_007_TestCases/TC_0703.java
@@ -27,6 +27,15 @@ public class TC_0703 extends TestBaseRapor {
         Assert.assertTrue(us_007_page.listOfHotelRoomsTitleText.isDisplayed(),"List of Hotel Rooms gozukmuyor");
         us_007_page.firstHotelRoomDetailBttn.click();
         Assert.assertEquals(Driver.getDriver().getTitle(),ConfigReader.getProperty("US_007_EditHotelRoomPageTitleText"),"Edit Hotel Room title gozukmuyor");
-
+        Assert.assertTrue(us_007_page.idHotelDropdownMenu.isEnabled()
+                &&us_007_page.codeBox.isEnabled()
+                &&us_007_page.nameBox.isEnabled()
+                &&us_007_page.locationBox.isEnabled()
+                &&us_007_page.descriptionTextArea.isEnabled()
+                &&us_007_page.priceBox.isEnabled()
+                &&us_007_page.idGroupRoomTypeDropdownMenu.isEnabled()
+                &&us_007_page.maxAdultCountBox.isEnabled()
+                &&us_007_page.maxChildCountBox.isEnabled()
+                &&us_007_page.isAvailableBox.isEnabled(),"DropMenu Unenabled");
     }
 }
